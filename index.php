7e13510b7cc01cf20e7fedc515925668f4d69b38
Orhan Esin <33238226+orhanesin@users.noreply.github.com>
2018-10-22 01:03:31 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require("main.php");
+
+$api = new main_api(dirname(__FILE__));
+
+$api->setHtmlAttr("lang","en");
+$api->setBodyAttr("class","main_api");
+$api->setThemplate_parts("body","body.php");
+$api->setHeader('title','<title>Welcome to PHTML</title>');
+
+$api->init();
+?>
