5af42b7cf9f38089da8f8d3601b6380250a85d3d
Orhan Esin <33238226+orhanesin@users.noreply.github.com>
2019-01-08 00:36:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $api = new main_api(dirname(__FILE__));
 $api->setHtmlAttr("lang","en");
 $api->setBodyAttr("class","main_api");
 $api->setThemplate_parts("body","body.php");
-$api->setHeader('title','<title>Welcome to PHTML</title>');
+$api->setHeader('title','<title>Welcome to Phplus</title>');
 
 $api->init();
 ?>
